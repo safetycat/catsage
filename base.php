88bf479e5990122fcaa36967e69ae9d6fe261415
elvis <elvina@safetycat.org>
2016-03-29 11:12:41 +0100

    add uk container-center to head and base

--- base.php
+++ base.php
@@ -18,8 +18,8 @@ use Roots\Sage\Wrapper;
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-    <div class="wrap container" role="document">
-      <div class="content row">
+    <div class="wrap uk-container uk-container-center" role="document">
+      <div class="content uk-grid">
         <main class="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
